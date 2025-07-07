4024fe7c5acaff14b4343142dd7a5653cc7aabd9
Jashan <jashandeep4329@gmail.com>
2025-07-07 22:55:23 +0530

    did the palindrome exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function (text) {
+
+    //the accepted letters in the palindrome
+    const acceptedLetters = "abcdefghijklmnopqrstuvwxyz1234567890"
+
+        //creating a clean string with the given string
+
+     const cleanedString = text.toLowerCase()
+        .split('')
+        .filter((character) => acceptedLetters.includes(character) )
+        .join('')
+
+        //creating a reversed string with the cleaned string
+        const reversedString = cleanedString.split('').reverse().join('');
+
+        //matching the strings to check if palindrome or not
+
+        return cleanedString === reversedString;
 
 };
 
