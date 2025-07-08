fd67c41a0fceea852a97abce33a8a35328c5a5c8
Jashan <jashandeep4329@gmail.com>
2025-07-08 15:32:05 +0530

    completed the fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
 
+    let count;
+    //first check if the passed argument is a number or not and if not then turn it into integer and put it in the current value
+
+    if(typeof countArg !== "number"){
+        
+        count = parseInt(countArg);
+    }else{
+        count = countArg;
+    }
+
+    //setup the edge cases 
+    if(count < 0) return "OOPS"
+    if(count == 0) return 0;
+
+    //the main logic to return the correct number to the user
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for(let i = 2; i <= count; i++) {
+        
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+    return firstPrev;
 };
 
 // Do not edit below this line
